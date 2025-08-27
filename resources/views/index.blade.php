52848c26776942e458c99dac0d232adeb266d8ca
rafalander <rafa.lander@hotmail.com>
2025-08-27 08:41:41 -0300

    Remove botao desnecessario do front

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -310,13 +310,6 @@
           >
             <div class="offcanvas-header">
               <h5 class="offcanvas-title" id="sidebarMenuLabel">Company name</h5>
-              <button
-                type="button"
-                class="btn-close"
-                data-bs-dismiss="offcanvas"
-                data-bs-target="#sidebarMenu"
-                aria-label="Close"
-              ></button>
             </div>
 
             <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
